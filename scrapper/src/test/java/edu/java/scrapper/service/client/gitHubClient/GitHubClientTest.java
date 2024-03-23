e8c105cbc6ec004beaff875878e772425c6f4013
Daniil Lokosov <lokosov.danil@yandex.ru>
2024-03-23 19:19:34 +0300

    added retry for all clients with tests

--- scrapper/src/test/java/edu/java/scrapper/service/client/gitHubClient/GitHubClientTest.java
+++ scrapper/src/test/java/edu/java/scrapper/service/client/gitHubClient/GitHubClientTest.java
@@ -4,30 +4,48 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.github.tomakehurst.wiremock.junit5.WireMockRuntimeInfo;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import edu.java.scrapper.configuration.ApplicationConfig;
 import edu.java.scrapper.configuration.ClientConfiguration;
 import edu.java.scrapper.service.client.gitHubClient.dto.IssueResponse;
 import edu.java.scrapper.service.client.gitHubClient.dto.RepositoryResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import java.net.URI;
+import java.time.Duration;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.util.List;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
+import static com.github.tomakehurst.wiremock.client.WireMock.status;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @WireMockTest
 class GitHubClientTest {
 
+    private static final Integer maxAttempts = 2;
     GitHubClient client;
 
     @BeforeEach
     void init(WireMockRuntimeInfo wm) {
-        ClientConfiguration clientConfiguration = new ClientConfiguration();
+        ApplicationConfig applicationConfig = new ApplicationConfig(
+            null,
+            null,
+            null,
+            new ApplicationConfig.RetryConfig(
+                maxAttempts,
+                ApplicationConfig.RetryConfig.DelayType.FIXED,
+                Duration.ofSeconds(1),
+                List.of(502)
+            )
+        );
+        ClientConfiguration clientConfiguration = new ClientConfiguration(applicationConfig);
 
         client = clientConfiguration.gitHubClient(wm.getHttpBaseUrl());
     }
@@ -94,6 +112,51 @@ class GitHubClientTest {
     @DisplayName("Некорректный запрос тикетов")
     void fetchTickets_shouldThrowExceptionForIncorrectRequest() {
         assertThrows(RuntimeException.class, () -> client.fetchIssues("error", "repo"));
+    }
+
+    @Test
+    @DisplayName("Ответ сервера предполагает retry, retry успешен")
+    void call_shouldRetryRequestForSpecificResponseCodeAndCompleteSuccessfulIfItNeededMaxRetries() {
+        for (int i = 0; i < maxAttempts; ++i) {
+            stubFor(get("/repos/owner/repo")
+                .willReturn(status(502)).inScenario("Test")
+                .whenScenarioStateIs(i == 0 ? STARTED : String.valueOf(i - 1))
+                .willSetStateTo(String.valueOf(i)));
+        }
+        stubFor(get("/repos/owner/repo")
+            .willReturn(okJson(
+                "{\"name\": \"test\", " +
+                    "\"description\": \"test\", " +
+                    "\"html_url\": \"https://github.com/test/test\", " +
+                    "\"updated_at\": \"2024-02-05T20:00:06Z\"}")).inScenario("Test")
+            .whenScenarioStateIs(String.valueOf(maxAttempts - 1)));
+
+        assertDoesNotThrow(() -> client.fetchRepository("owner", "repo"));
+    }
+
+    @Test
+    @DisplayName("Ответ сервера предполагает retry, retry неуспешен")
+    void call_shouldRetryRequestForSpecificResponseCodeAndCompleteUnsuccessfulIfItNeededToMoreThanMaxRetries() {
+        for (int i = 0; i <= maxAttempts; ++i) {
+            stubFor(get("/repos/owner/repo")
+                .willReturn(status(502)).inScenario("Test")
+                .whenScenarioStateIs(i == 0 ? STARTED : String.valueOf(i - 1))
+                .willSetStateTo(String.valueOf(i)));
+        }
+        stubFor(get("/repos/owner/repo")
+            .willReturn(okJson(
+                "{\"name\": \"test\", " +
+                    "\"description\": \"test\", " +
+                    "\"html_url\": \"https://github.com/test/test\", " +
+                    "\"updated_at\": \"2024-02-05T20:00:06Z\"}")).inScenario("Test")
+            .whenScenarioStateIs(String.valueOf(maxAttempts)));
+
+        Integer statusCode =
+            assertThrows(WebClientResponseException.class, () -> client.fetchRepository("owner", "repo"))
+                .getStatusCode().value();
+
+        assertThat(statusCode)
+            .isEqualTo(502);
     }
 
     public static String asJsonString(final Object obj) {
