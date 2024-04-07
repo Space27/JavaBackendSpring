8d7087b14e8f3fca9a393f4dc7b21100f1cd25c4
Daniil Lokosov <lokosov.danil@yandex.ru>
2024-04-07 14:51:21 +0300

    added counter for bot proceed messages

--- bot/src/main/java/edu/java/bot/service/CommandPool.java
+++ bot/src/main/java/edu/java/bot/service/CommandPool.java
@@ -4,17 +4,23 @@ import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import edu.java.bot.service.command.Command;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
 public class CommandPool {
 
     private static final String NO_SUPPORT_MESSAGE = "Команда не поддерживается или написана неправильно!";
 
     private final List<Command> commands;
+    private final Counter counter;
+
+    public CommandPool(List<Command> commands) {
+        this.commands = commands;
+        this.counter = Metrics.counter("proceed.messages");
+    }
 
     public List<BotCommand> getBotCommands() {
         return commands.stream()
@@ -23,12 +29,17 @@ public class CommandPool {
     }
 
     public SendMessage process(Update update) {
+        if (update == null || update.message() == null) {
+            return null;
+        }
+        counter.increment();
+
         for (Command command : commands) {
             if (command.supports(update)) {
                 return command.execute(update);
             }
         }
 
-        return update.message() != null ? new SendMessage(update.message().chat().id(), NO_SUPPORT_MESSAGE) : null;
+        return new SendMessage(update.message().chat().id(), NO_SUPPORT_MESSAGE);
     }
 }
